8fd04ba197438f2ac89670304eff775ed2abbce0
Maria Jose Olazarri <majo.olazarri@gmail.com>
2023-10-29 19:10:32 -0300

    primera entrega react

---
+++ pescalandia/src/components/CartWidget.jsx
@@ -0,0 +1,10 @@
+import cart from '../assets/—Pngtree—shopping cart_5394047.png';
+
+export const CartWidget = () => {
+    return (
+    <>
+    <img src = {cart} alt="carrito de compras" width="50" />
+    <span>7</span>
+    </> 
+    );
+};
---
+++ pescalandia/src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+import { CartWidget } from "./CartWidget";
+
+export const NavBar = () => {
+    return ( 
+    <Navbar bg="dark" data-bs-theme="dark">
+    <Container>
+      <Navbar.Brand href="#home">Pescalandia</Navbar.Brand>
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Catálogo</Nav.Link>
+        <Nav.Link href="#features">Accesorios</Nav.Link>
+        <Nav.Link href="#pricing">Contacto</Nav.Link>
+      </Nav>
+      <CartWidget/>
+    </Container>
+  </Navbar>
+    
+           
+    );
+};
