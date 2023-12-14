eaf8db23d881acde73f9d1c8818b769dbd48496e
Maria Jose Olazarri <majo.olazarri@gmail.com>
2023-12-14 10:54:19 -0300

    creando el context

--- pescalandia/src/components/ItemDetailsContainer.jsx
+++ pescalandia/src/components/ItemDetailsContainer.jsx
@@ -1,6 +1,7 @@
-import { useState, useEffect } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
+import Container from 'react-bootstrap/Container'; 
 import { productos } from "../data/productos";
 
 export const ItemDetailsContainer = () => {
