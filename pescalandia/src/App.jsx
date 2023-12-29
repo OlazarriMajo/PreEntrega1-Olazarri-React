484297edacb2746666d11dc0fffb71a5f4b43384
Maria Jose Olazarri <majo.olazarri@gmail.com>
2023-12-29 18:40:42 -0300

    rehaciendo el proyecto

--- pescalandia/src/App.jsx
+++ pescalandia/src/App.jsx
@@ -2,27 +2,36 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { NavBar } from "./components/NavBar";
 import { ItemListContainer } from "./components/ItemListContainer";
 import { ItemDetailsContainer } from "./components/ItemDetailsContainer";
+import Contacto from "./components/Contacto";
 
 import "./App.css";
 import { CartProvider } from "./contexts/CartContext";
+import Carrito from "./components/Carrito";
+import Checkout from "./components/Chechkout";
+
 
 function App() {
   
 
   return(
-   
+   <div>
+<CartProvider>
 <BrowserRouter>
 <NavBar/>
-<CartProvider>
+
 <Routes>
   <Route path="/" element={<ItemListContainer />} />
   <Route path="/category/:id" element={<ItemListContainer />} />
   <Route path="/item/:id" element={<ItemDetailsContainer/> } />
-  <Route path="*" element={<>ERROR!</> } />
+  <Route path="/contacto" element={<Contacto />}/>
+  <Route path="/carrito" element={<Carrito />}/>
+  <Route path="/checkout" element={<Checkout />}/>
+  <Route path="*" element={<>No hay productos encontrados</> } />
 </Routes>
-</CartProvider>
-</BrowserRouter>
 
+</BrowserRouter>
+</CartProvider>
+</div>
   );
   
 }
